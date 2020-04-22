8965b0fbb23bdb3d1c0b88fb7745382a0317fa42
Ilia <fortyhell@gmail.com>
2020-04-22 15:15:43 +0300

    Create class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+//ulong require ull in the end, max = 18446744073709551615ull
+
+class Magma {
+    ulong key;
+public:
+    Magma(ulong key): key{key}{};
+    ulong encrypt(ulong data){
+        return data;
+    }
+};
 
 int main(){
+    Magma a(0x3f43def);
+    ulong message = a.encrypt(0x3428);
+    cout << message;
     return 0;
 }
