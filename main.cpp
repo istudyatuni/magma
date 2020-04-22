368c237d257e6463ff1b2d196bf9f54ed6c24e53
Ilia <fortyhell@gmail.com>
2020-04-22 18:23:16 +0300

    Add int256 and splitting a block into 2 parts

--- main.cpp
+++ main.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
+
 //ulong require ull in the end, max = 18446744073709551615ull
+typedef unsigned long long ullong;
+typedef unsigned long ulong;
+
+class int256 {
+    ullong bits[4];
+public:
+    int256(ullong b0, ullong b1, ullong b2, ullong b3):bits{b0, b1, b2, b3}{}
+    ullong get(int i) {
+        return bits[i];
+    }
+};
 
 class Magma {
-    ulong key;
+    int256 key;
+    std::vector<char> v;
+    //ullong getFromTo(const ullong data, int left, int right) {
+
+    //}
+
 public:
-    Magma(ulong key): key{key}{};
-    ulong encrypt(ulong data){
-        return data;
+    Magma(int256 key): key{key}{};
+    ullong encrypt(const ullong input){
+        ullong left = input;
+        ullong right = left & 0xffffffff;
+        left >>= 32;
+        return right;
+    }
+    int256 getKey() {
+        return key;
     }
 };
 
 int main(){
-    Magma a(0x3f43def);
-    ulong message = a.encrypt(0x3428);
-    cout << message;
+    int256 key(0xffeeddccbbaa9988, 0x7766554433221100, 0xf0f1f2f3f4f5f6f7, 0xf8f9fafbfcfdfeff);
+    Magma a(key);
+    ullong message = a.encrypt(0b010101011010101);
+
     return 0;
 }
